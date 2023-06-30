2a8f85ade4c5a41676228e7616befff243293076
Romanchelys <purtov143@gmail.com>
2023-06-30 06:24:21 +0000

    broken

--- SalonLib.Test/ServiceLocatorTest.cs
+++ SalonLib.Test/ServiceLocatorTest.cs
@@ -13,19 +13,10 @@ public class ServiceLocatorTest
     [Fact]
     public void Service2Test()
     {
-        var expected = "В читальном зале";
+        var expected = "У рояля";
         var actual = SalonLib.ServiceLocator.GetService("Петь романсы");
         Assert.Equal(expected, actual);
     }
-
-    [Fact]
-    public void Service3Test()
-    {
-        var expected = "В читальном зале";
-        var actual = SalonLib.ServiceLocator.GetService("Писать статьи");
-        Assert.Equal(expected, actual);
-    }
-}
     public void Service3Test()
     {
         var expected = "В кабинете";
